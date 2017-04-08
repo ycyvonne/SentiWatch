d5b8d5ce7ed0db11c6626d54f96dfb3e79821687
Yvonne Chen <yc.yvonnechen@yahoo.com>
2017-04-08 16:14:23 -0700

    random chrome extension setup

---
+++ content.js
@@ -0,0 +1,28 @@
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+      console.log(firstHref);
+    }
+  }
+);
+
+
+chrome.storage.sync.set({'value': 'test'}, function() {
+  // Notify that we saved.
+  message('Settings saved');
+});
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+    for (key in changes) {
+      var storageChange = changes[key];
+      console.log('Storage key "%s" in namespace "%s" changed. ' +
+                  'Old value was "%s", new value is "%s".',
+                  key,
+                  namespace,
+                  storageChange.oldValue,
+                  storageChange.newValue);
+    }
+});
