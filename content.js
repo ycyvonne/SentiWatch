8ec6068a859494816823c55cc2620578b88c9b1e
Yvonne Chen <yc.yvonnechen@yahoo.com>
2017-04-09 00:43:02 -0700

    finished content storage and formatting in JSON

--- content.js
+++ content.js
@@ -9,46 +9,43 @@ chrome.runtime.onMessage.addListener(
   }
 );*/
 
-/*
-(function (chrome) {
-
-    var js = document.createElement('script');
-    js.type = 'text/javascript';
-    js.src = chrome.extension.getURL('inject.js');
-    document.getElementsByTagName('head')[0].appendChild(js);
-
-    var js2 = document.createElement('script');
-    js2.type = 'text/javascript';
-    js2.src = chrome.extension.getURL('jquery-3.2.0.min.js');
-    document.getElementsByTagName('head')[0].appendChild(js2);
-
-}(chrome));*/
-
+console.log('hi')
 var sentence = '';
 var currentSentence = '';
+var runningInput = '';
 
-$('input').each(function(index){
-	var elem = $(this);
-	// Save current value of element
-	elem.data('oldVal', elem.val());
+var currentId = 1;
 
-	// Look for changes in the value
-	elem.bind("propertychange change click keyup input paste", function(event){
+var params = {
+    // Request parameters
+};
 
-		// If value has changed...
-		if (elem.data('oldVal') != elem.val()) {
 
-			// Updated stored value
-			elem.data('oldVal', elem.val());
+var allData = {
+  data: [] //{time: '', text: '', sentiment: '', keywords: ''}
+};
 
-			// Do action
-			updateSentence(elem.val(), '.');
-		}
-   });
+document.addEventListener(
+  'keypress',
+  function (ev) {
+  	if(ev.key.length == 1)
+   		runningInput += ev.key;
+   	updateSentence(runningInput, '.', false);
+  },
+  true
+);
 
-});
+document.onkeypress = function(e){
+    if (!e) e = window.event;
+    var keyCode = e.keyCode || e.which;
+    if (keyCode == '13'){
+      // Enter pressed
+      updateSentence(runningInput, '.', true);
+      return false;
+    }
+}
 
-function updateSentence(val, delim){
+function updateSentence(val, delim, override){
 	var index = val.lastIndexOf(delim);
 
 	var secondlastIndex = index != -1
@@ -57,7 +54,7 @@ function updateSentence(val, delim){
 
 	currentSentence = '';
 
-	if(val.substr(val.length - 1) == delim){
+	if(val.substr(val.length - 1) == delim || override){
 
 		if(secondlastIndex != -1)
 			currentSentence = val.substr(secondlastIndex + 1);
@@ -66,16 +63,112 @@ function updateSentence(val, delim){
 
 		console.log('currentSentence', currentSentence)
 		sentence += currentSentence + ' ';
-		
-		//make api request
-		console.log('sentence: ' + sentence);
+
+    getSentiment(currentSentence, setStorage);
 
 		currentSentence = '';
+
 	}
 }
 
+function getSentiment(currentSentence, callback){
 
-chrome.storage.sync.set({'value': 'fvgbhj'}, function() {
+  var data = {
+    documents: [{id: currentId++, text: currentSentence}]
+  };
+
+  var dataJSON = JSON.stringify(data);
+
+  $.ajax({
+      url: "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment?" + $.param(params),
+      beforeSend: function(xhrObj){
+          // Request headers
+          xhrObj.setRequestHeader("Content-Type","application/json");
+          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","5cd0b00a05ad44e4a3fbdd36c673bf4b");
+      },
+      type: "POST",
+      // Request body
+      data: dataJSON,
+  })
+  .done(function(data) {
+      getKeyWords(currentSentence, callback, data);
+  });
+}
+
+function getKeyWords(currentSentence, callback, dataSoFar){
+  var data = {
+    documents: [{id: currentId, text: currentSentence}]
+  };
+
+  var dataJSON = JSON.stringify(data);
+
+  $.ajax({
+      url: "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases?" + $.param(params),
+      beforeSend: function(xhrObj){
+          // Request headers
+          xhrObj.setRequestHeader("Content-Type","application/json");
+          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","5cd0b00a05ad44e4a3fbdd36c673bf4b");
+      },
+      type: "POST",
+      // Request body
+      data: dataJSON,
+  })
+  .done(function(data) {
+    var keyPhrases = data.documents[0].keyPhrases;
+    callback(currentSentence, keyPhrases, dataSoFar);
+  });
+
+}
+
+function setStorage(currentSentence, keyPhrases, data){
+  var scoreRaw = data.documents[0].score;
+  var positive = scoreRaw > 0.5;
+  var score = positive ? scoreRaw : 1 - scoreRaw; //0 to 1
+
+  var d = new Date(); // for now
+
+  var week  = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+  var currentObject = {
+    id: data.documents[0].id,
+    text: currentSentence,
+    isPositive: positive,
+    score: {
+      raw: scoreRaw,
+      absolute: score
+    },
+    keyPhrases: keyPhrases,
+    time: {
+      year: d.getFullYear(),
+      month: {
+        number: d.getMonth(),
+        text: month[d.getMonth()]
+      },
+      weekDay: {
+        number: d.getDay(), //0-6
+        text: week[d.getDay()] //sun -> saturday
+      },
+      hours: d.getHours(),
+      minutes: d.getMinutes(),
+      seconds: d.getSeconds() 
+    }
+  }
+
+  chrome.storage.sync.get('data', function(data) {
+
+    allData = data.data.trim().length != 0 ? JSON.parse(data.data) : {data: []};
+    allData.data.push(currentObject);
+    chrome.storage.sync.set({'data': JSON.stringify(allData)}, function() {
+    });
+    console.log('current data stored: ', allData);
+
+  });
+  
+}
+
+//reset for now
+chrome.storage.sync.set({'data': ''}, function() {
 });
 
 chrome.storage.onChanged.addListener(function(changes, namespace) {
