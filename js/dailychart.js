3e60a84e27b68dd12c94e6d32884f7469d175a08
Yvonne Chen <yc.yvonnechen@yahoo.com>
2017-04-09 06:59:53 -0700

    added chart stuff

--- js/dailychart.js
+++ js/dailychart.js
@@ -8,22 +8,60 @@ for(var i = 0; i < 6; i++){
     xArr.push(hour - (5 - i));
 }
 
+loadChartData();
+
+function loadChartData(){
+  console.log('loading');
+  chrome.storage.sync.get('data', function(data) {
+    parseData(JSON.parse(data.data));
+  });
+}
+
+function parseData(data){
+  console.log('json data', data.data)
+  var compiledData = [];
+
+  for(var i = 0; i < data.data.length; i++)
+  {
+    var curObj = data.data[i];
+    if(curObj.time.year == date.getFullYear()
+        && curObj.time.month.number == date.getMonth()
+        && ((curObj.time.hours <= date.getHours() || 24 - curObj.time.hours + date.getHours() < 6)
+            && (curObj.time.hours > date.getHours() - 6) || 24 + curObj.time.hours - date.getHours() < 6))
+    {
+        compiledData.push({
+          hour: curObj.time.hours,
+          value: curObj.score.absolute * 100
+        });
+    }
+  }
+  console.log('compiled data')
+  loadChart(compiledData);
+}
+
+var positive = 140;
+var negative = 110;
+
+$('.header').click(function(){
+    positive += 10;
+})
+
 var chart = c3.generate({
     bindto: '#dailychart',
     data: {
         x: 'x',
         columns: [
             xArr,
-            ['positive', 300, 350, 300, 10, 20, 70],
-            ['negative', 130, 100, 140, 200, 150, 50]
+            ['negative', 30, 40, 60, 120, 150, negative],
+            ['positive', 40, 60, 30, 60, 80, positive]
         ],
         types: {
-            positive: 'area-spline',
-            negative: 'area-spline'
+            negative: 'area-spline',
+            positive: 'area-spline'
         },
          colors: {
-           positive: '#53b6f1',
-           negative: '#da5c5c'
+           negative: '#da5c5c',
+           positive: '#53b6f1'
          }
     },
     axis: {
