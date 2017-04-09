3477ecf6913ddf3ddd70af53ae1d2c49eab393bc
Yvonne Chen <yc.yvonnechen@yahoo.com>
2017-04-09 05:53:01 -0700

    added graph stuff

--- js/dailychart.js
+++ js/dailychart.js
@@ -1,7 +1,19 @@
+var date = new Date();
+var hour = date.getHours();
+
+
+var xArr = ['x'];
+
+for(var i = 0; i < 6; i++){
+    xArr.push(hour - (5 - i));
+}
+
 var chart = c3.generate({
     bindto: '#dailychart',
     data: {
+        x: 'x',
         columns: [
+            xArr,
             ['positive', 300, 350, 300, 0, 0, 0],
             ['negative', 130, 100, 140, 200, 150, 50]
         ],
@@ -13,5 +25,13 @@ var chart = c3.generate({
            positive: '#53b6f1',
            negative: '#da5c5c'
          }
+    },
+    axis: {
+        y: {
+            label: { // ADD
+              text: 'Sentiment Score',
+              position: 'outer-middle'
+            }
+          }
     }
 });
