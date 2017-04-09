752568b8b42cbc4d72c8957c2a4d48fdbff2e849
insomnolent <jbcccsun@gmail.com>
2017-04-08 23:19:52 -0700

    minor changes

--- logging.js
+++ logging.js
@@ -1,6 +1,7 @@
 // key: 5058506ffa3545ebaf23d921722c9164
 document.addEventListener('keydown', function(event) {
   var key = event.keyCode;
-    if (key == 99) {
-    changeColor;
+    if (key == 46) { // if letter is a period
+      
+    }
 }
