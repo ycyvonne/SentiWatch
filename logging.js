09eec480e7d798267bdcb38763f756d42d643e11
insomnolent <jbcccsun@gmail.com>
2017-04-09 00:14:50 -0700

    added graph to Daily tab

--- logging.js
+++
@@ -1,7 +0,0 @@
-// key: 5058506ffa3545ebaf23d921722c9164
-document.addEventListener('keydown', function(event) {
-  var key = event.keyCode;
-    if (key == 46) { // if letter is a period
-      
-    }
-}
